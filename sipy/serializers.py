b09c0ed901acdf3fe87404ec95cf4541a6a8a43f
Nithin Senthilvel <nithin.senthilvel@gmail.com>
2026-06-14 10:12:21 -0400

    profile serializer changed to accept react native request data format

--- sipy/serializers.py
+++ sipy/serializers.py
@@ -11,15 +11,19 @@ class UserSerializer(serializers.ModelSerializer):
 
 class ProfileSerializer(serializers.ModelSerializer):
     '''Convert abstract python data objects into JSON to send to the front end'''
-    user = UserSerializer()
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'first_name', 'last_name', 'bio_text', 'profile_image']
+        fields = ['id', 'username', 'password', 'first_name', 'last_name', 'bio_text', 'profile_image']
 
     def create(self, validated_data):
         '''Override the create method to also create a user'''
-        user_data = validated_data.pop('user')
+        user_data = {
+            'username': validated_data.pop('username'),
+            'password': validated_data.pop('password')
+        }
         user = User.objects.create_user(username=user_data['username'], password=user_data['password'])
         profile = Profile.objects.create(user=user, **validated_data)
         return profile
